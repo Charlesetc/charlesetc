8392af2656831f7efc85d62374dc391a382080d5
Charles Chamberlain <charlespipin@gmail.com>
2017-01-18 22:32:02 -0600

    update to master from development

--- js/master.js
+++ js/master.js
@@ -1,54 +1,72 @@
 // master.js
 
+// removing 'no-js' class
+document.body.parentElement.className = 'nothing here'
+
 if (document.location.hash == "#back") {
-  document.location.hash = "#";
-  elements = $('.site-header');
-  elements.removeClass('site-header-transition');
-  elements.removeClass('vertical-header');
-  elements.css("opacity", "1");
-  setTimeout(function() {
+    $('.site-header').removeClass('site-header-transition')
+    $('.site-header').removeClass('vertical-header');
+}
+
+$(document).ready(function() {
+
+  // $('.site-header').css("opacity", "0");
+
+  if (document.location.hash == "#back") {
+    console.log("hi");
+    document.location.hash = "#";
+    elements = $('.site-header');
+    elements.addClass('site-header-transition');
+    elements.addClass('vertical-header');
+    elements.css("opacity", "1");
+    // elements.animate({"opacity": "1"}, 10, 'linear');
+    // setTimeout(function() {
     elements.addClass('site-header-transition');
     
+    // setTimeout(function() {
+    //   $('.site-header').addClass('vertical-header');
+    // }, 1000);
+    // // }, 10);
+
+    $('#main-links').css('opacity', '0');
+    setTimeout(function() { 
+      $('#main-links').animate({
+        opacity: 1,
+      }, 200, 'linear');
+    }, 300);
+  } else {
+    $('.site-header').removeClass('site-header-transition');
+    $('.site-header').css('opacity', '1');
+    $('#main-links').css('opacity', '1');
     setTimeout(function() {
-      $('.site-header').addClass('vertical-header');
-    });
-  }, 10);
+      $('.site-header').addClass('site-header-transition');
+    }, 10);
+  }
 
-  $('#main-links').css('opacity', '0');
-  setTimeout(function() { 
+  function transition(to) {
+    $('.site-header').removeClass('vertical-header');
     $('#main-links').animate({
-      opacity: 1,
+      opacity: 0,
     }, 200, 'linear');
-  }, 300);
-} else {
-  $('.site-header').removeClass('site-header-transition');
-  $('.site-header').css('opacity', '1');
-  $('#main-links').css('opacity', '1');
-  setTimeout(function() {
-    $('.site-header').addClass('site-header-transition');
-  }, 10);
-}
+    document.location.hash = "#back"
+    setTimeout(function () {
+      document.location.pathname = to
+    }, 520);
+  }
 
-function transition(to) {
-  $('.site-header').removeClass('vertical-header');
-  $('#main-links').animate({
-    opacity: 0,
-  }, 200, 'linear');
-  document.location.hash = "#back"
-  setTimeout(function () {
-    document.location.pathname = to
-  }, 520);
-}
+  $('.jekyll-link').removeAttr('href')
+  $('.jekyll-link').on('click', function () {
+    transition('/write/');
+  })
 
-$('.jekyll-link').on('click', function () {
-  transition('/write/');
-})
+  $('.etc-link').removeAttr('href')
+  $('.etc-link').on('click', function () {
+    transition('/etc/');
+  })
 
-$('.etc-link').on('click', function () {
-  transition('/etc/');
-})
+  $('.site-header').click(function () {
+    // just because the hash is ugly
+    return(false);
+  });
 
-$('.site-header').click(function () {
-  // just because the hash is ugly
-  return(false);
 });
